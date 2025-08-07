cefb43b92280a91a0ce9beddcddc4706b4b13b55
Reksa Maulana <161690316+Ecaks25@users.noreply.github.com>
2025-08-07 16:37:23 +0700

    Add table to Barang Jadi pages

--- resources/views/grinding/barang-jadi.blade.php
+++ resources/views/grinding/barang-jadi.blade.php
@@ -1,9 +1,25 @@
 @section('title', __('Grinding Barang Jadi'))
 <x-layouts.app :title="__('Grinding Barang Jadi')">
   <div class="card">
-    <div class="card-body">
+    <div class="card-header">
       <h5 class="card-title mb-0">{{ __('Barang Jadi') }}</h5>
-      <p>{{ __('Belum ada data') }}</p>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive text-nowrap">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>{{ __('Nama Produk') }}</th>
+              <th>{{ __('QTY') }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colspan="2" class="text-center">{{ __('Belum ada data') }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </x-layouts.app>
